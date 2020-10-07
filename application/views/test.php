e561250fecbe5ce23f4a101e8c6064ceb8de7b62
TerenceSimplon <desdi0406>
2020-10-07 22:33:16 +0200

    Page contact : Mettre en place l'envoi d'email en utiliser le Mailer fourni dans l'exercice 4 du Niveau 2 PHP-MySQL

--- application/views/test.php
+++ application/views/test.php
@@ -1,10 +1,3 @@
 <?php
-//print_r($db);
-$counter = 0 ;
-foreach ($db->getDatabase() as $item) {
-	//echo $item->getCreationDateYear() . " ". $item->getCreationDateFull()."<br>";
-	//echo ($counter++)." : " ;
-	var_dump($item->getParticipants()) ;
-	//echo "<br>" ;
-}
+//send_mail("terence.ferut@gmail.com", "test email", "super email") ;
 ?>
