a9962e1290f69f5565aa5676be657c3905c011d0
Thomas <62775569+GroovyHooked@users.noreply.github.com>
2020-10-09 15:55:04 +0200

    Add files via upload
    Modifs comportement loupe

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <nav class="navbar">
-                    <a href="<?= base_url("/") ?>"><img class="img-cerisy" src="<?= base_url("/resources/img/logo-cerisy-200.png") ?>" alt="Les colloques CERISY"></a>
-                    <form class="form-inline" action="<?= base_url("/rechercher") ?>" method="GET">
-                        <p class="title-search">Recherche</p>
-                        <input type="search" name="search" class="form-control" placeholder="Date, intervenant, titre..." aria-label="Recherche">
+                <nav class="navbar w-100 d-flex flex-row justify-content-between">
+                    <a href="<?= base_url("/") ?>"><img class="img-cerisy" src="<?= base_url("/resources/img/img-1.jpg") ?>" alt="Les colloques CERISY"></a>
+                    <form class="form-inline d-flex flex-row flex-nowrap" action="<?= base_url("/rechercher") ?>" method="GET">
+                        <p class="title-search d-none d-sm-block">Recherche</p>
+                        <input type="search" name="search" class="form-control d-none d-sm-block" placeholder="Date, intervenant, titre..." aria-label="Recherche">
                         <button class="btn-search btn " type="submit"><i class="fa fa-search"></i></button>
                     </form>
                 </nav>
