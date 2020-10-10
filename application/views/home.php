6eace2999d64faf52bf96a733dae575c6a3a5fe5
Stephane0713 <stephane-dupre@outlook.com>
2020-10-10 09:03:03 +0200

    ajout de classes dans home.php pour alternance gauche/droite

--- application/views/home.php
+++ application/views/home.php
@@ -1,35 +1,34 @@
 	<div class="container text-center">
 <?php
-	foreach ($timelines as $date_array) {
-		$edition = $date_array[0]["edition"] ;
-		$date = $date_array[0]["date"] ;
-?>
+foreach ($timelines as $date_array) {
+    $edition = $date_array[0]["edition"];
+    $date = $date_array[0]["date"];
+    ?>
 
-			<div class="custom-edition my-5">
-				<div class="row">
-					<div class="col-12">
-						<div class="custom-date text-center">
-							<p><?= $edition ?></p>
-							<h3 class="title"><?= $date ?></h3>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center">
-					<div class="col-12 col-md-6 col-lg-4 col-xl-3">
-						<?php
-							foreach ($date_array as $conference) {
-								$datas = array();
-								$datas["timeline_id"] = $conference["id"];
-								$datas["timeline_name"] = $conference["nom"];
-								$datas["thematiques"] = $db->getThematiquesByTimeline()[$conference["id"]];
-								$this->load->view('templates/card-timeline', $datas);
-							}
-						?>
-					</div>
+	<div class="custom-edition my-5">
+		<div class="row custom-edition-row">
+			<div class="col-12">
+				<div class="custom-date text-center">
+					<p><?=$edition?></p>
+					<h3 class="title"><?=$date?></h3>
 				</div>
 			</div>
+		</div>
+		<?php foreach ($date_array as $conference) {?>
+		<div class="row justify-content-center custom-card-row">
+			<div class="col-12 col-md-6 col-lg-4 col-xl-3">
+				<?php
+$datas = array();
+        $datas["timeline_id"] = $conference["id"];
+        $datas["timeline_name"] = $conference["nom"];
+        $datas["thematiques"] = $db->getThematiquesByTimeline()[$conference["id"]];
+        $this->load->view('templates/card-timeline', $datas);?>
+	</div>
+</div>
+    <?php }?>
+	</div>
 <?php
-	}
+}
 ?>
 	</div>
 
